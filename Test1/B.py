c16a5556b7847c650086ce1a3e45d8df27b79fd2
Mitya <dim.maluckow2017@yandex.ru>
2025-02-16 22:07:21 +0300

    solved two problem on binary search

--- Test1/B.py
+++ Test1/B.py
@@ -1,13 +1,28 @@
 def binary_search(arr, x):
   l = -1
   r = len(arr)
+  
+  ans = [10 ** 10, 10 ** 10]
   while r - l > 1:
     m = (l + r) // 2
-    if arr[m] <= x:
+    k = abs(arr[m] - x)
+    
+    if k <= ans[0]:
+      if k < ans[0]:
+        ans[1] = arr[m]
+      if k == ans[0]:
+        ans[1] = min(ans[1], arr[m])
+      ans[0] = k
+    
+    if arr[m] == x:
+      return arr[m]
+    
+    if arr[m] < x:
       l = m
     else:
       r = m
-  return arr[l]
+
+  return ans[1]
 
 N, K = map(int,input().split())
 a = list(map(int,input().split()))
