1869f5e82f2e900b6d0f775404e0936c4d3c9e6a
Mitya <dim.maluckow2017@yandex.ru>
2025-02-19 00:06:59 +0300

    solved C and F problems

--- Test1/C.py
+++ Test1/C.py
@@ -1,6 +1,28 @@
 import sys
-n = int(input())
+
+def binary_search(n):
+    l = 1
+    r = n
+    while r - l >= 0:
+        m = (l + r) // 2
+        m = min(m, n)
+        ans = query(m)
+
+        if ans == "<":
+            r = m - 1
+        else:
+            l = m + 1
+
+    if l <= n and query(l) == "<":
+        m = l - 1
+
+    print(f"! {m}")
+
+
 def query(x):
-  print(x)
-  sys.stdout.flush()
-  return input()
+    print(x)
+    sys.stdout.flush()
+    return input()
+
+n = int(input())
+binary_search(n)
--- Test1/F.py
+++ Test1/F.py
@@ -4,25 +4,31 @@ def merge_sort(arr):
   ind = len(arr) // 2
   left = merge_sort(arr[:ind])
   right = merge_sort(arr[ind:])
-  return merge(left,right)
+  return merge(left, right)
 
-def merge(left,right):
+def merge(left, right):
+  global inv
   res = []
   i, j = 0, 0
   while i < len(left) and j < len(right):
     if left[i] <= right[j]:
       res.append(left[i])
-      i+=1
+      i += 1
+      inv += j
     else:
       res.append(right[j])
-      j+=1
-  
+      j += 1
   if i < len(left):
     res.extend(left[i:])
+    inv += len(left[i:]) * j
   if j < len(right):
     res.extend(right[j:])
   return res
 
+inv = 0
 N = int(input())
-arr = list(map(int,input().split()))
-print(*merge_sort(arr))
+ls = list(map(int,input().split()))
+ls = merge_sort(ls)
+print(inv)
+print(*ls)
+
